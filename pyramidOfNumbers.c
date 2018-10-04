d3d78327772c6bf1f016a9ea036a828f6c1d55cf
simonHellrigl <simsi.hellrigl@gmail.com>
2018-10-04 09:32:06 +0200

    works a little bit

--- pyramidOfNumbers.c
+++ pyramidOfNumbers.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+struct BigInt{
+  int digits[160];
+  int digitCount =0;
+};
+
 
 int main(int argc, char const *argv[]) {
-  /* code */
-  
+  const int MAX_DIGITS=160;
+  char userinput[MAX_DIGITS];
+  int lenght;
+
+  printf("Pyramid of numbers");
+  printf("Please enter a number: ");
+  scanf("%s", &userinput);
+  lenght = strlen(userinput);
+  printf("%d\n", lenght);
+
   return 0;
 }
